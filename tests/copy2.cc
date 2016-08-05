eb87c4e06328585c80c25a6537e92e2ddaa0c9c8
Felix Salfelder <felix@salfelder.org>
2016-08-06 00:52:05 +0100

    add missing test

---
+++ tests/copy2.cc
@@ -0,0 +1,67 @@
+
+#include <assert.h>
+#include <boost/functional/hash.hpp> // BUG?!
+#include <boost/graph/adjacency_matrix.hpp>
+#include "../boost.h"
+#include <boost/graph/graph_traits.hpp>
+
+template<class G>
+struct dvv_config : public gala::graph_cfg_default<G>
+{
+	 typedef boost::mpl::true_ is_directed_t;
+};
+
+typedef gala::graph<std::vector, std::vector, uint16_t, dvv_config> sg_dvv16;
+typedef gala::graph<std::vector, std::vector, uint16_t> sg_dvu;
+typedef gala::graph<std::set, std::vector, uint16_t> SSG_16i;
+typedef boost::adjacency_list <boost::vecS, boost::vecS, boost::directedS> BALD_t;
+typedef boost::adjacency_list <boost::vecS, boost::vecS, boost::undirectedS> BALUVEC_t;
+typedef boost::adjacency_list <boost::setS, boost::vecS, boost::undirectedS> BALU_t;
+
+
+using namespace std;
+
+int main(int , char* [])
+{
+	using namespace boost;
+
+	BALD_t GD;
+	BALU_t GU;
+
+	sg_dvv16 G2(5);
+
+	add_edge(0, 1, G2);
+	add_edge(1, 0, G2);
+	add_edge(1, 2, G2);
+	add_edge(2, 0, G2);
+	assert(boost::num_vertices(G2)==5);
+	assert(boost::num_edges(G2)==4);
+
+	boost::copy_graph(G2, GD);
+
+	assert(boost::num_vertices(GD)==5);
+	assert(boost::num_edges(GD)==4);
+
+	boost::copy_graph(G2, GU);
+
+	assert(boost::num_vertices(GU)==5);
+	assert(boost::num_edges(GU)==3);
+
+	sg_dvu GU2;
+
+	boost::copy_graph(G2, GU2);
+
+	assert(boost::num_vertices(GU2)==5);
+	assert(boost::num_edges(GU2)==3);
+
+	BALUVEC_t GU3;
+	boost::copy_graph(G2, GU3);
+
+	add_edge(2, 0, G2);
+	add_edge(2, 0, G2);
+	add_edge(2, 0, G2);
+	add_edge(2, 0, G2);
+	assert(boost::num_vertices(GU3)==5);
+	assert(boost::num_edges(GU3)==4); // double edge 1-0 and 0-1. no multiple 2-0...
+
+}
